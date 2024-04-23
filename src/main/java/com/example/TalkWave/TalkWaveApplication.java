0a3b83a0a73f67bcbddf9e4e6cf944fdaac6fdb0
hardiksolanki24 <hardiksolanki200224@gmail.com>
2024-04-23 20:47:52 +0530

    Project Structure Push to Repo

---
+++ src/main/java/com/example/TalkWave/TalkWaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.TalkWave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkWaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkWaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TalkWave/TalkWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TalkWave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
